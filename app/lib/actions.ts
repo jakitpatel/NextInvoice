1fb7b4bfa0354cbfaa534147bb34423e3ab75431
JAkit Patel <jakitpatel@gmail.com>
2023-11-07 19:28:43 +0530

    Added create/update/delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -65,7 +65,7 @@ export async function updateInvoice(invoiceId: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
+    //throw new Error('Failed to Delete Invoice');
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
